714402a75ad20afa3cecd9de72a81a86744fd493
Gray Smith <gray.smith00@gmail.com>
2018-08-27 15:14:57 -0600

    Build out redux boilerplate and basic file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
