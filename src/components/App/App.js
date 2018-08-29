5a58de2b2275f38a0673aa5258a1b054c923222f
sojurner <pykim0591@gmail.com>
2018-08-28 20:06:40 -0600

    add Navlink, switch and route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, NavLink, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { getNowPlaying, populateSearch } from "../../helpers.js";
 
-import { getNowPlaying } from '../../helpers.js';
-
-import './App.css';
+import "./App.css";
+import CardContainer from "../card-container/CardContainer.js";
 
 class App extends Component {
-  async componentDidMount() {
-    const nowPlaying = await getNowPlaying();
-    console.log(nowPlaying);
+  constructor() {
+    super();
+    this.state = {
+      activeTab: ""
+    };
   }
 
+  componentDidMount() {
+    this.props.getNowPlaying();
+  }
+
+  handleClick = event => {
+    const { name } = event.target;
+    this.setState({ activeTab: name });
+  };
+
   render() {
-    const navigationLink = ['Now Playing', 'Favorites', 'Movies with Pauls'];
-    const displayLinks = navigationLink.map(link => (
-      <NavLink className="nav-button" name={link} exact to={`/${link}`}>
+    const { nowPlaying, activeTab } = this.state;
+    const navigationLink = ["NowPlaying", "Favorites", "MoviesWithPauls"];
+    const displayLinks = navigationLink.map((link, index) => (
+      <NavLink
+        key={`${link}-${index}`}
+        className="nav-bar"
+        name={link}
+        exact
+        to={`/${link}`}
+        onClick={event => this.handleClick(event)}
+      >
         {link}
       </NavLink>
     ));
@@ -22,9 +42,28 @@ class App extends Component {
       <div className="App">
         <h1>App</h1>
         {displayLinks}
+        <Switch>
+          <Route exact path="/" />
+          <Route
+            exact
+            path={`/${activeTab}`}
+            render={({ match }) => {
+              const { path } = match;
+              const slicedPath = path.slice(1);
+              return <CardContainer path={slicedPath} data={nowPlaying} />;
+            }}
+          />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  getNowPlaying: () => dispatch(getNowPlaying())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
