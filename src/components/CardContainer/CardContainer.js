f1376a3b6f9283b070824682e817eb621b5ea2cd
Gray Smith <gray.smith00@gmail.com>
2018-08-29 17:01:53 -0600

    Pass movie object to MovieCard as an object instead of destructuring

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -5,14 +5,15 @@ import PropTypes from 'prop-types';
 
 const CardContainer = ({ movies, category }) => {
   const displayCards = movies[category].map(movie => (
-    <MovieCard key={movie.id} {...movie} />
+    <MovieCard key={movie.id} movie={movie} />
   ));
 
   return <div>{displayCards}</div>;
 };
 
 CardContainer.propTypes = {
-  movies: PropTypes.object.isRequired
+  movies: PropTypes.object.isRequired,
+  category: PropTypes.string.isRequired
 };
 
 const mapStateToProps = state => ({
