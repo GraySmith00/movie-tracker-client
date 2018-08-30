530ff4e331e0c5b69e186ff22cd91efba90397a5
Cody Taft <cody.taft@gmail.com>
2018-08-30 15:10:56 -0600

    Fix populate search function

--- src/helpers.js
+++ src/helpers.js
@@ -38,54 +38,61 @@ export const populateSearch = async input => {
   const url = `https://api.themoviedb.org/3/search/person?api_key=${key}&query=${input}`;
   const searchResponse = await fetch(url);
   const searchResult = await searchResponse.json();
-  // const searchedPaul = await searchforPaul(searchResult);
+  const searchedPaul = await searchforPaul(searchResult);
+  // console.log(searchedPaul);
   // return searchedPaul;
   return searchResult;
 };
 
-// export const searchforPaul = async paulResponse => {
-//   const paulMovies = await paulResponse.results.map(async response => {
-//     const knownPaulMovies = await response.known_for.map(async movie => {
-//       const paulMovies = await getPaulMovies(movie.id);
-//       return paulMovies;
-//     });
-//     return await Promise.all([...knownPaulMovies]);
-//   });
+export const searchforPaul = async paulResponse => {
+  const paulMovies = await paulResponse.results.map(async response => {
+    const knownPaulMovies = await response.known_for.map(async movie => {
+      if (movie.media_type === 'movie') {
+        const paulMovies = await getPaulMovies(movie.id);
+        return paulMovies;
+      }
+    });
+    return await Promise.all([...knownPaulMovies]);
+  });
 
-//   const paulPromises = await Promise.all([...paulMovies]);
-//   scrapePaulMovies(paulPromises);
-// };
+  const paulPromises = await Promise.all([...paulMovies]);
+  scrapePaulMovies(paulPromises);
+};
 
-// const getPaulMovies = async id => {
-//   if (!id) return;
-//   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
-//   const response = await fetch(url);
-//   const result = await response.json();
-//   return await scrapeGetPaul(result);
-// };
+const getPaulMovies = async id => {
+  if (!id) return;
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
+  const response = await fetch(url);
+  const result = await response.json();
+  return await scrapeGetPaul(result);
+};
 
-// const scrapeGetPaul = async result => {
-//   const trailer = await getMovieTrailer(result.id);
-//   return {
-//     Id: result.id,
-//     Title: result.title,
-//     "Realease Date": result.release_date,
-//     Overview: result.overview,
-//     Img: `http://image.tmdb.org/t/p/original${result.poster_path}`,
-//     Trailer: `https://www.youtube.com/embed/${trailer.results[0].key}`,
-//     Rating: result.vote_average,
-//     favorite: false
-//   };
-// };
+const scrapeGetPaul = async result => {
+  const trailer = await getMovieTrailer(result.id);
+  return {
+    Id: result.id,
+    Title: result.title,
+    'Realease Date': result.release_date,
+    Overview: result.overview,
+    Img: `http://image.tmdb.org/t/p/original${result.poster_path}`,
+    // Trailer: `https://www.youtube.com/embed/${trailer.results[0].key}`,
+    Rating: result.vote_average,
+    favorite: false
+  };
+};
 
-// const scrapePaulMovies = async movies => {
-//   return movies.reduce((allMovies, movie) => {
-//     movie.forEach(movi => {
-//       movi.Id ? (allMovies = [...allMovies, movi]) : null;
-//     });
-//     return allMovies;
-//   }, []);
-// };
+const scrapePaulMovies = async movies => {
+  console.log(movies);
+  return movies.reduce((allMovies, movie) => {
+    movie.forEach(movi => {
+      if (movi) {
+        movi.Id ? (allMovies = [...allMovies, movi]) : null;
+      }
+    });
+    console.log(allMovies);
+    return allMovies;
+  }, []);
+};
 
 export const registerUser = async user => {
   if (!user.email) {
