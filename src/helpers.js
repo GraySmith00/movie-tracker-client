2bf8a200633710ae6bee1598dc7f45daba07a06b
sojurner <pykim0591@gmail.com>
2018-08-30 10:32:46 -0600

    Add favorites action

--- src/helpers.js
+++ src/helpers.js
@@ -43,49 +43,49 @@ export const populateSearch = async input => {
   return searchResult;
 };
 
-export const searchforPaul = async paulResponse => {
-  const paulMovies = await paulResponse.results.map(async response => {
-    const knownPaulMovies = await response.known_for.map(async movie => {
-      const paulMovies = await getPaulMovies(movie.id);
-      return paulMovies;
-    });
-    return await Promise.all([...knownPaulMovies]);
-  });
+// export const searchforPaul = async paulResponse => {
+//   const paulMovies = await paulResponse.results.map(async response => {
+//     const knownPaulMovies = await response.known_for.map(async movie => {
+//       const paulMovies = await getPaulMovies(movie.id);
+//       return paulMovies;
+//     });
+//     return await Promise.all([...knownPaulMovies]);
+//   });
 
-  const paulPromises = await Promise.all([...paulMovies]);
-  scrapePaulMovies(paulPromises);
-};
+//   const paulPromises = await Promise.all([...paulMovies]);
+//   scrapePaulMovies(paulPromises);
+// };
 
-const getPaulMovies = async id => {
-  if (!id) return;
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
-  const response = await fetch(url);
-  const result = await response.json();
-  return await scrapeGetPaul(result);
-};
+// const getPaulMovies = async id => {
+//   if (!id) return;
+//   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   return await scrapeGetPaul(result);
+// };
 
-const scrapeGetPaul = async result => {
-  // const trailer = await getMovieTrailer(result.id);
-  return {
-    Id: result.id,
-    Title: result.title,
-    "Realease Date": result.release_date,
-    Overview: result.overview,
-    Img: `http://image.tmdb.org/t/p/original${result.poster_path}`,
-    // Trailer: `https://www.youtube.com/embed/${trailer.results[0].key}`,
-    Rating: result.vote_average,
-    favorite: false
-  };
-};
+// const scrapeGetPaul = async result => {
+//   const trailer = await getMovieTrailer(result.id);
+//   return {
+//     Id: result.id,
+//     Title: result.title,
+//     "Realease Date": result.release_date,
+//     Overview: result.overview,
+//     Img: `http://image.tmdb.org/t/p/original${result.poster_path}`,
+//     Trailer: `https://www.youtube.com/embed/${trailer.results[0].key}`,
+//     Rating: result.vote_average,
+//     favorite: false
+//   };
+// };
 
-const scrapePaulMovies = async movies => {
-  const x = movies.reduce((allMovies, movie) => {
-    movie.forEach(movi => {
-      return movi.Id ? (allMovies = [...allMovies, movi]) : null;
-    });
-    return allMovies;
-  }, []);
-};
+// const scrapePaulMovies = async movies => {
+//   return movies.reduce((allMovies, movie) => {
+//     movie.forEach(movi => {
+//       movi.Id ? (allMovies = [...allMovies, movi]) : null;
+//     });
+//     return allMovies;
+//   }, []);
+// };
 
 export const registerUser = async user => {
   if (!user.email) {
@@ -168,4 +168,12 @@ export const addFavorite = async (movie, currentUser) => {
     }
   );
   console.log(response);
+};
+
+export const getFavorites = async currentUser => {
+  console.log(currentUser);
+  // const response = await fetch(`/api/users/${user_id}/favorites`);
+  // const favorites = await response.json();
+  // console.log(favorites);
+  // return favorites;
 };
