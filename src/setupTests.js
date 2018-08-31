b76d40a30ca21df42394f6a8cbdb67a5bcb5d9c8
Cody Taft <cody.taft@gmail.com>
2018-08-31 17:33:34 -0600

    Fix Setup test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,10 @@
-import Enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({
+  adapter: new Adapter()
+  // disableLifecycleMethods: true
+});
 
 // class LocalStorage {
 //   constructor() {
