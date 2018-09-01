7b4d7183f16472a1092de0b774ac3688e5b53afe
sojurner <pykim0591@gmail.com>
2018-09-01 09:08:56 -0600

    Rename files and folders of navigation and routes

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { setCurrentUser } from '../../actions/userActions';
+import { clearFavorites } from '../../actions/movieActions';
+
+class Navigation extends Component {
+  logoutUser = () => {
+    const { setCurrentUser, clearFavorites } = this.props;
+    setCurrentUser(null);
+    clearFavorites();
+  };
+  render() {
+    return (
+      <nav className="nav-btns">
+        <NavLink exact to="/" className="nav-link">
+          Home
+        </NavLink>
+        <NavLink exact to="/login" className="nav-link">
+          Login
+        </NavLink>
+        <NavLink exact to="/register" className="nav-link">
+          Sign Up
+        </NavLink>
+        <NavLink
+          exact
+          to="/"
+          className="nav-link nav-link-logout"
+          onClick={this.logoutUser}
+        >
+          Logout
+        </NavLink>
+        <NavLink exact to="/favorites" className="nav-link">
+          Favorites
+        </NavLink>
+      </nav>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+  clearFavorites: () => dispatch(clearFavorites())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Navigation);
