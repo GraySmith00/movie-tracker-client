7f092b7688f12485b597bbbb45ffe4fb22fe33bd
Gray Smith <gray.smith00@gmail.com>
2018-09-02 17:04:46 -0600

    Clean up Navigation component and fix conditional NavLinks

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import { setCurrentUser } from '../../actions/userActions';
@@ -7,13 +8,6 @@ import { clearFavorites } from '../../actions/movieActions';
 import './Navigation.css';
 
 export class Navigation extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     isLoggedIn: false
-  //   };
-  // }
-
   logoutUser = () => {
     const { setCurrentUser, clearFavorites, history } = this.props;
     setCurrentUser(null);
@@ -21,16 +15,7 @@ export class Navigation extends Component {
     history.replace('/');
   };
 
-  // checkUserStatus = () => {
-  //   if (this.props.currentUser) {
-  //     this.setState({
-  //       isLoggedIn: true
-  //     });
-  //   }
-  // };
-
   render() {
-    console.log(this.props.currentUser);
     return (
       <section className="navigation">
         <Link exact to="/" className="nav-link brand">
@@ -38,20 +23,21 @@ export class Navigation extends Component {
           MovieTracker
         </Link>
         <nav className="nav-btns">
-          <NavLink exact to="/favorites" className="nav-link">
-            <i className="fas fa-heart" />
-            Favorites
-          </NavLink>
-
           {this.props.currentUser !== null ? (
-            <NavLink
-              exact
-              to="/"
-              className="nav-link nav-link-logout"
-              onClick={this.logoutUser}
-            >
-              Logout
-            </NavLink>
+            <div>
+              <NavLink exact to="/favorites" className="nav-link">
+                <i className="fas fa-heart" />
+                Favorites
+              </NavLink>
+              <Link
+                exact
+                to="/"
+                className="nav-link nav-link-logout"
+                onClick={this.logoutUser}
+              >
+                Logout
+              </Link>
+            </div>
           ) : (
             <div>
               <NavLink
@@ -60,6 +46,7 @@ export class Navigation extends Component {
                 className="nav-link"
                 onClick={this.checkUserStatus}
               >
+                <i className="fas fa-user-plus" />
                 Sign Up
               </NavLink>
               <NavLink
@@ -78,6 +65,12 @@ export class Navigation extends Component {
     );
   }
 }
+
+Navigation.propTypes = {
+  setCurrentUser: PropTypes.func.isRequired,
+  clearFavorites: PropTypes.func.isRequired,
+  currentUser: PropTypes.object.isRequired
+};
 
 export const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)),
