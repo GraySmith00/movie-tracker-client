9a357ed1b6a6e0bf6bd1943a61c5b7047a91c395
Gray Smith <gray.smith00@gmail.com>
2018-09-02 18:52:25 -0600

    Hard code out styling for Jumbotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import './Jumbotron.css';
+import StarRatingComponent from 'react-star-rating-component';
 
-const Jumbotron = () => {
+import './Jumbotron.css';
+import { connect } from 'react-redux';
+
+const Jumbotron = ({ nowPlayingMovies }) => {
+  const jumboMovies = nowPlayingMovies.slice(0, 1);
+
   return (
     <div
       className="jumbotron"
@@ -9,8 +14,43 @@ const Jumbotron = () => {
         backgroundImage:
           'url(' + require(`../../images/jumbotron/345940.jpg`) + ')'
       }}
-    />
+    >
+      <div className="jumbotron-overlay">
+        <div className="inner-content">
+          <div className="jumbo-text-container">
+            <h1>The Meg</h1>
+            <p className="jumbo-overview">
+              A deep sea submersible pilot revisits his past fears in the
+              Mariana Trench, and accidentally unleashes the seventy foot
+              ancestor of the Great White Shark believed to be extinct.
+            </p>
+            <p className="jumbo-release-date">Released on 2018-06-07</p>
+            <div style={{ fontSize: '24px' }}>
+              <StarRatingComponent
+                name="rate2"
+                editing={false}
+                renderStarIcon={() => <span>★</span>}
+                starCount={10}
+                value={5}
+              />
+            </div>
+          </div>
+          <section className="arrow-icons-container">
+            <div className="backArrow">
+              <i className="fas fa-angle-left jumbo-arrow" />
+            </div>
+            <div className="nextArrow">
+              <i className="fas fa-angle-right jumbo-arrow" />
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default Jumbotron;
+const mapStateToProps = state => ({
+  nowPlayingMovies: state.movies.nowPlaying
+});
+
+export default connect(mapStateToProps)(Jumbotron);
