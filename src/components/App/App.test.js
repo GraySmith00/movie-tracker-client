daf5ec394a21d3385adb256839738384f0855821
Cody Taft <cody.taft@gmail.com>
2018-09-03 10:41:58 -0600

    Clean console logs

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -53,7 +53,6 @@ describe('App', () => {
         currentUser={mockStore.currentUser}
       />
     );
-    // console.log(window.fetch);
     await expect(wrapper).rejects.toEqual(error);
   });
 
