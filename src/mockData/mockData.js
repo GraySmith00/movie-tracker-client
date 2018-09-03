18451bd4f331db542f4e73490f599ece6d315981
Cody Taft <cody.taft@gmail.com>
2018-09-03 10:39:53 -0600

    Finish CardContainer test

--- src/mockData/mockData.js
+++ src/mockData/mockData.js
@@ -513,3 +513,229 @@ export const mockVideoDetailsFetch = {
     }
   ]
 };
+
+export const mockMovies = {
+  nowPlaying: [
+    {
+      movie_id: 402900,
+      title: "Ocean's Eight",
+      release_date: '2018-06-07',
+      overview:
+        "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century at New York's annual Met Gala.",
+      vote_average: 7,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 353081,
+      title: 'Mission: Impossible - Fallout',
+      release_date: '2018-07-25',
+      overview:
+        'When an IMF mission ends badly, the world is faced with dire consequences. As Ethan Hunt takes it upon himself to fulfil his original briefing, the CIA begin to question his loyalty and his motives. The IMF team find themselves in a race against time, hunted by assassins while trying to prevent a global catastrophe.',
+      vote_average: 7.3,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/AkJQpZp9WoNdj7pLYSj1L0RcMMN.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 345940,
+      title: 'The Meg',
+      release_date: '2018-08-09',
+      overview:
+        'A deep sea submersible pilot revisits his past fears in the Mariana Trench, and accidentally unleashes the seventy foot ancestor of the Great White Shark believed to be extinct.',
+      vote_average: 6.2,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/xqECHNvzbDL5I3iiOVUkVPJMSbc.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 507569,
+      title: 'The Seven Deadly Sins: Prisoners of the Sky',
+      release_date: '2018-08-18',
+      overview:
+        'The Seven Deadly Sins travel to a remote land in search of the phantom ingredient "sky fish." Meliodas and Hawk end up at a "Sky Palace" that exists above the clouds, where all the residents have wings. Meliodas is mistaken for a boy who committed a crime and is thrown in prison. Meanwhile, the residents are preparing a ceremony for defense against a ferocious beast that awakens once every 3,000 years. But the Six Knights of Black, a Demon Clan army, arrives and removes the seal on the beast in order to threaten the lives of the residents of Sky Palace. Meliodas and his allies meet the Six Knights of Black in battle.',
+      vote_average: 6.1,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/fK6zf4eN5xxkCO3MP9rDNWOq6Ys.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 363088,
+      title: 'Ant-Man and the Wasp',
+      release_date: '2018-07-04',
+      overview:
+        "As Scott Lang awaits expiration of his term of house detention, Hope van Dyne and Dr. Hank Pym involve him in a scheme to rescue Mrs. van Dyne from the micro-universe into which she has fallen, while two groups of schemers converge on them with intentions of stealing Dr. Pym's inventions.",
+      vote_average: 7,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/rv1AWImgx386ULjcf62VYaW8zSt.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 399360,
+      title: 'Alpha',
+      release_date: '2018-08-17',
+      overview:
+        'After a hunting expedition goes awry, a young caveman struggles against the elements to find his way home.',
+      vote_average: 5.7,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/afdZAIcAQscziqVtsEoh2PwsYTW.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 493922,
+      title: 'Hereditary',
+      release_date: '2018-06-04',
+      overview:
+        "When Ellen, the matriarch of the Graham family, passes away, her daughter's family begins to unravel cryptic and increasingly terrifying secrets about their ancestry. The more they discover, the more they find themselves trying to outrun the sinister fate they seem to have inherited.",
+      vote_average: 7,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/4GFPuL14eXi66V96xBWY73Y9PfR.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 500664,
+      title: 'Upgrade',
+      release_date: '2018-06-01',
+      overview:
+        'A brutal mugging leaves Grey Trace paralyzed in the hospital and his beloved wife dead. A billionaire inventor soon offers Trace a cure — an artificial intelligence implant called STEM that will enhance his body. Now able to walk, Grey finds that he also has superhuman strength and agility — skills he uses to seek revenge against the thugs who destroyed his life.',
+      vote_average: 7.4,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/woLGROUdPWdBnLVFKYdGD3kFkPY.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 455980,
+      title: 'Tag',
+      release_date: '2018-05-30',
+      overview:
+        'For one month every year, five highly competitive friends hit the ground running in a no-holds-barred game of tag they’ve been playing since the first grade. This year, the game coincides with the wedding of their only undefeated player, which should finally make him an easy target. But he knows they’re coming...and he’s ready.',
+      vote_average: 6.9,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/eXXpuW2xaq5Aen9N5prFlARVIvr.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 260513,
+      title: 'Incredibles 2',
+      release_date: '2018-06-14',
+      overview:
+        'Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.',
+      vote_average: 7.7,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/x1txcDXkcM65gl7w20PwYSxAYah.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 429300,
+      title: 'Adrift',
+      release_date: '2018-05-31',
+      overview:
+        "A true story of survival, as a young couple's chance encounter leads them first to love, and then on the adventure of a lifetime as they face one of the most catastrophic hurricanes in recorded history.",
+      vote_average: 6.5,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/5gLDeADaETvwQlQow5szlyuhLbj.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 442249,
+      title: 'The First Purge',
+      release_date: '2018-07-04',
+      overview:
+        'To push the crime rate below one percent for the rest of the year, the New Founding Fathers of America test a sociological theory that vents aggression for one night in one isolated community. But when the violence of oppressors meets the rage of the others, the contagion will explode from the trial-city borders and spread across the nation.',
+      vote_average: 6.1,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/2slvblTroiT1lY9bYLK7Amigo1k.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 345887,
+      title: 'The Equalizer 2',
+      release_date: '2018-07-19',
+      overview:
+        "Robert McCall returns to deliver his special brand of vigilante justice -- but how far will he go when it's someone he loves?",
+      vote_average: 6.3,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/cQvc9N6JiMVKqol3wcYrGshsIdZ.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 447200,
+      title: 'Skyscraper',
+      release_date: '2018-07-11',
+      overview:
+        "Framed and on the run, a former FBI agent must save his family from a blazing fire in the world's tallest building.",
+      vote_average: 6,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/5LYSsOPzuP13201qSzMjNxi8FxN.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 455207,
+      title: 'Crazy Rich Asians',
+      release_date: '2018-08-15',
+      overview:
+        "An American-born Chinese economics professor accompanies her boyfriend to Singapore for his best friend's wedding, only to get thrust into the lives of Asia's rich and famous.",
+      vote_average: 7,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/gnTqi4nhIi1eesT5uYMmhEPGNih.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 420814,
+      title: 'Christopher Robin',
+      release_date: '2018-08-02',
+      overview:
+        'Working-class family man Christopher Robin encounters his childhood friend Winnie-the-Pooh, who helps him to rediscover the joys of life.',
+      vote_average: 7.4,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/xR5w0he6czZkcAz459a4iPBqXGe.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 476292,
+      title: 'Maquia: When the Promised Flower Blooms',
+      release_date: '2018-02-24',
+      overview:
+        "A story of encounters and partings interwoven between people; this is a human drama with feelings that touch one's heart gradually, which everyone has experienced at least once.",
+      vote_average: 5.1,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/qcnOKCPleLOTWPPgYI0YT1MOQwR.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 487558,
+      title: 'BlacKkKlansman',
+      release_date: '2018-08-09',
+      overview:
+        'Ron Stallworth, an African-American police officer from Colorado, successfully manages to infiltrate the local Ku Klux Klan and become the head of the local chapter.',
+      vote_average: 7.6,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/55W6mUVv4CXMMQHHhV2zXtLSpXQ.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 507562,
+      title: 'Crayon Shin-chan: Burst Serving! Kung Fu Boys ~Ramen Rebellion~',
+      release_date: '2018-04-13',
+      overview:
+        'Shin-chan is all set to challenge Kung Fu in the Chinatown of Kasukabe city, known as Aiyā Town.',
+      vote_average: 4.8,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/qjxtWgMzZbkViDqmbjXuuZCj1az.jpg',
+      favorite: false
+    },
+    {
+      movie_id: 400155,
+      title: 'Hotel Transylvania 3: Summer Vacation',
+      release_date: '2018-06-30',
+      overview:
+        "Dracula, Mavis, Johnny and the rest of the Drac Pack take a vacation on a luxury Monster Cruise Ship, where Dracula falls in love with the ship's captain, Ericka, who's secretly a descendant of Abraham Van Helsing, the notorious monster slayer.",
+      vote_average: 7,
+      poster_path:
+        'http://image.tmdb.org/t/p/original/gjAFM4xhA5vyLxxKMz38ujlUfDL.jpg',
+      favorite: false
+    }
+  ],
+  favorites: []
+};
